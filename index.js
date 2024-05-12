1cd910954c2e809a53b87eac43aa37fc198c633e
smasha11 <ismail.hussein.1120@gmail.com>
2024-05-12 15:33:24 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield" ];
+console.log(cats);
+
+function destructivelyAppendCat(name) {
+  cats.push('Ralph');
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat (name) {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat (name) {
+    cats.shift();
+}
+
+function appendCat(name) {
+  return [...cats, "Broom"];
+}
+function  prependCat(name) {
+    return ["Arnold", ...cats];
+}
+
+
+
+
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1, cats.length);
+}
